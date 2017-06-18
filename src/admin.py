1bf2ae5e2f65852528c6271e247495a1faf1a5c7
wes <wjak56@gmail.com>
2017-06-17 20:34:21 -0400

    comment Admin class

--- src/admin.py
+++ src/admin.py
@@ -4,12 +4,14 @@ class Admin:
     def __init__(self):
         return
 
+    # admin user is always authenticated for everything so it's a constant
     def is_authenticated(self):
         return True
 
     def is_active(self):
         return True
 
+    # we don't have anonymous users on this site
     def is_anonymous(self):
         return False
 
