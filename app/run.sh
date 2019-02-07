c90418c7bbf34c91cd7dfdad6be369aa65001463
Wesley Kerfoot <wjak56@gmail.com>
2019-02-06 22:14:50 -0500

    use env var for database name

--- app/run.sh
+++ app/run.sh
@@ -6,6 +6,6 @@ while [ $? != 0 ]; do
   sleep 5;
 done
 
-curl -XPUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@localhost:5984/blog || true
-curl -XPUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@localhost:5984/blog/_design/blogPosts -d @/blogPosts.json || true
+curl -XPUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@localhost:5984/$COUCHDB_NAME || true
+curl -XPUT http://$COUCHDB_USER:$COUCHDB_PASSWORD@localhost:5984/$COUCHDB_NAME/_design/blogPosts -d @/blogPosts.json || true
 exec python3 "$@"
